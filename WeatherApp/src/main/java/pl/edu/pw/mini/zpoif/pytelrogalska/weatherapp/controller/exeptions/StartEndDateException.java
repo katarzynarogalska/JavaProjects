9c7927e78e177c8a11f562d16756c1d4bbf121d1
Kasia Rogalska <kasiarogal03@gmail.com>
2024-02-07 15:30:12 +0100

    Aplikacja pogodowa

---
+++ WeatherApp/src/main/java/pl/edu/pw/mini/zpoif/pytelrogalska/weatherapp/controller/exeptions/StartEndDateException.java
@@ -0,0 +1,6 @@
+package pl.edu.pw.mini.zpoif.pytelrogalska.weatherapp.controller.exeptions;
+
+public class StartEndDateException extends Exception{ //własny błąd dla daty startowej późniejszej niż początkowa
+    public StartEndDateException() {
+    }
+}
---
+++ WeatherApp/src/test/java/pl/edu/pw/mini/zpoif/pytelrogalska/weatherapp/WeatherAppApplicationTest.java
@@ -0,0 +1,13 @@
+package pl.edu.pw.mini.zpoif.pytelrogalska.weatherapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class WeatherAppApplicationTest {
+
+    @Test
+    void context() {
+    }
+}
