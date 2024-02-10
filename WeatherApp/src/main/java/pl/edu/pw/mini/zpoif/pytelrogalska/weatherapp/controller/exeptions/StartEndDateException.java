81ea1f6bf4d76fe6907cc0a4fe488a8cf3217468
katarzynarogalska <126283808+katarzynarogalska@users.noreply.github.com>
2024-02-10 20:26:21 +0100

    Delete WeatherApp directorytest

--- WeatherApp/src/main/java/pl/edu/pw/mini/zpoif/pytelrogalska/weatherapp/controller/exeptions/StartEndDateException.java
+++
@@ -1,6 +0,0 @@
-package pl.edu.pw.mini.zpoif.pytelrogalska.weatherapp.controller.exeptions;
-
-public class StartEndDateException extends Exception{ //własny błąd dla daty startowej późniejszej niż początkowa
-    public StartEndDateException() {
-    }
-}
